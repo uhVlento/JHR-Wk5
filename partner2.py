a2bd25e589b1006ac128f75d3359dfb0eaab7ab1
uhVlento <jesusvh224@gmail.com>
2026-02-16 12:05:45 -0600

    This is crazy stuff

---
+++ partner2.py
@@ -0,0 +1,19 @@
+import random
+"""
+Partner 1: Kaleb De La Cruz
+Partner 2: Jesus Hernandez-Reyes
+"""
+
+print("Hello, World!")
+
+#Buddy did NOT make rock paper scissors
+# def play_game():
+#     options = ["rock", "paper", "scissors"]
+#     while True:
+#         user_choice = input("What is your hand? ")
+    
+        
+#     if user_choice not in options:
+#         print("Not an option")
+#         continue
+#     computer_choice = random.choice(options)
