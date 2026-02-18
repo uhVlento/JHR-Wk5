7d1352a9b4e7d7e628cd303b962e2f48546f4f02
uhVlento <jesusvh224@gmail.com>
2026-02-18 11:57:55 -0600

    Co-authored-by: badrelijun <badrelijun@users.noreply.github.com>

---
+++ partner1_02-18.py
@@ -0,0 +1,53 @@
+# 1. Print a welcome message
+print("Hello there!")
+
+# 2. Create a variable with your name
+name = "Jesus Hernandez-Reyes"
+
+# 3. Print the name
+print(name)
+
+# 4. Create a variable with your age
+age = 18
+
+# 5. Print the age
+print(age)
+
+# 6. Ask the user for their favorite color
+color = input("What is your favorite color? ")
+
+# 7. Print a sentence using the color
+RED = "\033[31m"
+RESET = '\033[0m'
+print(f"{RED} {color} is a cool color {RESET}")
+
+# 8. Create two number variables
+num=5
+num2=10
+
+# 9. Add the numbers
+num3=num+num2
+
+# 10. Print the result
+print(num,"+",num2,"=",num3)
+
+# 11. Check if age is greater than 18
+print("What is your age")
+age=int(input())
+
+# 12. Print message if true
+if age>18:
+    print("You are older than 18")
+# 13. Otherwise print another message
+# 14. Print message if false
+else:
+    print("You are younger than 18")
+
+
+# 15. Print a goodbye message
+print("Goodbye")
+
+#Resources used:
+#Stack Overflow
+
+
