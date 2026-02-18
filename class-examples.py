6972dde2487753ea5bdfc2e7262d11a1859f1608
uhVlento <jesusvh224@gmail.com>
2026-02-18 11:58:04 -0600

    Completed partner-1 for 2/18/2026

---
+++ class-examples.py
@@ -0,0 +1,30 @@
+#Example 1
+name = "Jesus"
+
+#Example 2
+a = 3
+
+#Example 3
+print("Hello World")
+
+#Example 4
+print(name)
+
+#Example 5
+print(f"Hello, {name}")
+
+#Example 6
+name = "not Jesus"
+print(f"Hello, {name}")
+
+#Example 7 
+animal = input("What is your favorite animal? ")
+print(f"{animal} is pretty cool")
+
+#Example 8
+if a > 10: #cause the numerical variable is from Example 2
+    print("a is bigger than 10")
+elif a == 10:
+    print("a is 10")
+else:
+    print("a is less than 10")
